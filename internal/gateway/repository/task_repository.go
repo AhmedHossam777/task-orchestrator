5afea9f750dddba26b7ba266661f1c21657a6be2
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-04-06 03:35:57 +0200

    feat: add service

--- internal/gateway/repository/task_repository.go
+++ internal/gateway/repository/task_repository.go
@@ -1,1 +1,77 @@
 package repository
+
+import (
+	"fmt"
+	"sync"
+	
+	"github.com/AhmedHossam777/task-orchestrator/internal/gateway/model"
+)
+
+type TaskRepository interface {
+	Create(task *model.Task) error
+	GetById(id string) (*model.Task, error)
+	List() ([]*model.Task, error)
+	Delete(id string) error
+}
+
+var (
+	ErrTaskNotFound      = fmt.Errorf("task not found")
+	ErrTaskAlreadyExists = fmt.Errorf("task already exists")
+)
+
+type inMemoryTaskRepository struct {
+	mu    sync.RWMutex
+	tasks map[string]*model.Task
+}
+
+func NewInMemoryTaskRepository() *inMemoryTaskRepository {
+	return &inMemoryTaskRepository{
+		tasks: make(map[string]*model.Task),
+	}
+}
+
+func (r *inMemoryTaskRepository) Create(task *model.Task) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	_, exits := r.tasks[task.ID]
+	if exits {
+		return ErrTaskAlreadyExists
+	}
+	r.tasks[task.ID] = task
+	return nil
+}
+
+func (r *inMemoryTaskRepository) GetById(id string) (*model.Task, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+	
+	task, exists := r.tasks[id]
+	if !exists {
+		return nil, ErrTaskNotFound
+	}
+	
+	return task, nil
+}
+
+func (r *inMemoryTaskRepository) List() ([]*model.Task, error) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+	tasks := make([]*model.Task, 0, len(r.tasks))
+	for _, task := range r.tasks {
+		tasks = append(tasks, task)
+	}
+	
+	return tasks, nil
+}
+
+func (r *inMemoryTaskRepository) Delete(id string) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	
+	if _, exists := r.tasks[id]; !exists {
+		return ErrTaskNotFound
+	}
+	
+	delete(r.tasks, id)
+	return nil
+}
--- internal/gateway/service/task_service.go
+++ internal/gateway/service/task_service.go
@@ -1,1 +1,77 @@
 package service
+
+import (
+	"crypto/rand"
+	"fmt"
+	"time"
+	
+	"github.com/AhmedHossam777/task-orchestrator/internal/gateway/model"
+	"github.com/AhmedHossam777/task-orchestrator/internal/gateway/repository"
+)
+
+type TaskService interface {
+	CreateTask(task model.CreateTaskRequest) (*model.Task, error)
+	GetTask(id string) (*model.Task, error)
+	ListTasks() ([]*model.Task, error)
+	DeleteTask(id string) error
+}
+
+type taskService struct {
+	repo repository.TaskRepository
+}
+
+func NewTaskService(repo repository.TaskRepository) TaskService {
+	return &taskService{
+		repo,
+	}
+}
+
+func (s *taskService) CreateTask(task model.CreateTaskRequest) (
+	*model.Task, error,
+) {
+	newTask := &model.Task{
+		ID:        generateID(),
+		Type:      task.Type,
+		Payload:   task.Payload,
+		Status:    model.TaskStatusPending,
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+	}
+	
+	err := s.repo.Create(newTask)
+	if err != nil {
+		return nil, fmt.Errorf("creating task: %w", err)
+	}
+	
+	return newTask, nil
+}
+
+func (s *taskService) GetTask(id string) (*model.Task, error) {
+	task, err := s.repo.GetById(id)
+	if err != nil {
+		return nil, fmt.Errorf("getting task %s: %w", id, err)
+	}
+	return task, nil
+}
+
+func (s *taskService) ListTasks() ([]*model.Task, error) {
+	tasks, err := s.repo.List()
+	if err != nil {
+		return nil, fmt.Errorf("listing tasks: %w", err)
+	}
+	return tasks, nil
+}
+
+func (s *taskService) DeleteTask(id string) error {
+	err := s.repo.Delete(id)
+	if err != nil {
+		return fmt.Errorf("Deleting Task %s: %w", id, err)
+	}
+	return nil
+}
+
+func generateID() string {
+	b := make([]byte, 12)
+	_, _ = rand.Read(b)
+	return fmt.Sprintf("task_%x", b)
+}
