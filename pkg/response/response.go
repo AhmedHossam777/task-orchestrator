9ca195b6e38965318740e7394399759a0d125c7c
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-04-06 02:37:36 +0200

    feat:add response

--- pkg/response/response.go
+++ pkg/response/response.go
@@ -1,1 +1,48 @@
 package response
+
+import (
+	"net/http"
+	
+	"github.com/gin-gonic/gin"
+)
+
+type Envelope struct {
+	Success bool   `json:"success"`
+	Data    any    `json:"data,omitempty"`
+	Error   *Error `json:"error,omitempty"`
+}
+
+type Error struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+func OK(c *gin.Context, data any) {
+	c.JSON(
+		http.StatusOK, Envelope{
+			Success: true,
+			Data:    data,
+		},
+	)
+}
+
+func Created(c *gin.Context, data any) {
+	c.JSON(
+		http.StatusCreated, &Envelope{
+			Success: true,
+			Data:    data,
+		},
+	)
+}
+
+func Fail(c *gin.Context, httpStatus int, code string, message string) {
+	c.JSON(
+		httpStatus, &Envelope{
+			Success: false,
+			Error: &Error{
+				Code:    code,
+				Message: message,
+			},
+		},
+	)
+}
