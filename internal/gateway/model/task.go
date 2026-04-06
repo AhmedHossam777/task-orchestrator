be0980f5bfb238f7b8b95fd4812d6c4187131485
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-04-06 02:30:14 +0200

    feat:add task model

--- internal/gateway/model/task.go
+++ internal/gateway/model/task.go
@@ -1,1 +1,30 @@
 package model
+
+import "time"
+
+type TaskStatus string
+
+const (
+	TaskStatusPending   TaskStatus = "Pending"
+	TaskStatusRunning   TaskStatus = "Running"
+	TaskStatusCompleted TaskStatus = "Completed"
+	TaskStatusFailed    TaskStatus = "Failed"
+)
+
+type Task struct {
+	ID          string
+	Type        string
+	Payload     map[string]any
+	Status      TaskStatus
+	Result      map[string]any
+	Error       string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+	CompletedAt *time.Time // Pointer because it's nullable
+}
+
+// ? create task request represent what it needed to create a task
+type CreateTaskRequest struct {
+	Type    string
+	Payload map[string]any
+}
