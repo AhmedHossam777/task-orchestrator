d0a6b06215a97a4bc9fa984494d53f5c8488bb96
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-04-09 15:48:40 +0200

    feat:add request_id middleware

--- internal/gateway/middleware/request_id.go
+++ internal/gateway/middleware/request_id.go
@@ -1,1 +1,29 @@
 package middleware
+
+import (
+	"crypto/rand"
+	"fmt"
+	
+	"github.com/gin-gonic/gin"
+)
+
+const RequestIDKey = "request_id"
+
+func RequestID() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		requestId := c.GetHeader("X-Request-ID")
+		if requestId == "" {
+			requestId = generateRequestID()
+		}
+		c.Set(RequestIDKey, requestId)
+		c.Header("X-Request-ID", requestId)
+		
+		c.Next()
+	}
+}
+
+func generateRequestID() string {
+	b := make([]byte, 8)
+	_, _ = rand.Read(b)
+	return fmt.Sprintf("req_%x", b)
+}
